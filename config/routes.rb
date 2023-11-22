5a1c97ad57cb1452f36f1c3d82025e43c4c2f3ee
David <dmaine6711@gmail.com>
2023-11-22 14:38:05 -0500

    finish phase 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # resources :users
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'users/', to: 'users#index' # as: 'users'  
+  post 'users/', to: 'users#create' 
+  get 'users/new', to: 'users#new'  #what dis 
+  get 'users/:id/edit/', to: 'users#edit'  #what dis 
+  get 'users/:id', to: 'users#show'
+  patch 'users/:id', to: 'users#update'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
+
+
 end
